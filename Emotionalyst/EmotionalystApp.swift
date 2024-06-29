625b79b9baf835c8ff72e6ca70750932b50850e5
HiHoi <ghtjd5087@gmail.com>
2024-06-29 10:53:57 +0900

    env: initial setting

---
+++ Emotionalyst/EmotionalystApp.swift
@@ -0,0 +1,32 @@
+//
+//  EmotionalystApp.swift
+//  Emotionalyst
+//
+//  Created by Hosung Lim on 6/29/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct EmotionalystApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
