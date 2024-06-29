4bea4f1ad683b37d18dc82840349fbf3e08044ea
UY <scutiuys@gmail.com>
2024-06-29 14:18:51 +0900

    [feat] create base recording view

---
+++ Emotionalyst/View/RecordingView.swift
@@ -0,0 +1,35 @@
+//
+//  RecordingView.swift
+//  Emotionalyst
+//
+//  Created by 신의연 on 6/29/24.
+//
+
+import SwiftUI
+
+struct RecordingView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            HStack {
+                WaveBar(isPlay: true, high: 1)
+                WaveBar(isPlay: true, high: 2)
+                WaveBar(isPlay: true, high: 1)
+                WaveBar(isPlay: true, high: 4)
+                WaveBar(isPlay: true, high: 3)
+                WaveBar(isPlay: true, high: 1)
+                WaveBar(isPlay: true, high: 2)
+                WaveBar(isPlay: true, high: 3)
+            }
+            Spacer()
+            RecordingButton {
+                
+            }
+        }
+        .padding(10)
+    }
+}
+
+#Preview {
+    RecordingView()
+}
